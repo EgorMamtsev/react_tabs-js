3da3f6f5ce9e15464bf1bcfe503d4aa540db3d70
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-05-28 11:12:36 -0500

    done

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,22 @@
-export const Tabs = () => {};
+export const Tabs = ({ tab, selected, activeTabId, setActiveTabId }) => {
+  if (!tab) {
+    return null;
+  }
+
+  const isActive = tab.id === activeTabId;
+
+  return (
+    <li className={isActive ? 'is-active' : ''} data-cy="Tab">
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={() => {
+          selected(tab);
+          setActiveTabId(tab.id);
+        }}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
